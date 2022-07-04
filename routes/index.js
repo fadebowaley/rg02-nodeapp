245ac63cb500bc39eb4f4f1865e769fdc242ff08
Ademola Adebowale <me@fadebowaley.me>
2022-07-04 02:02:50 +0100

    2 days Project

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* Get all the major 8 pages in the router.
+history
+events
+quicklinks
+help ministries
+contact us */
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/event", function (req, res, next) {
+  res.render("index", { title: "Event Page" });
+});
+
+
+router.get("/history", function (req, res, next) {
+  res.render("index", { title: "history" });
+});
+
+router.get("/quicklinks", function (req, res, next) {
+  res.render("index", { title: "quick links" });
+});
+
+router.get("/help-ministries", function (req, res, next) {
+  res.render("index", { title: "help ministries" });
+});
+
+router.get("/contact-us", function (req, res, next) {
+  res.render("index", { title: "contact us" });
+});
+
+
+module.exports = router;
+
